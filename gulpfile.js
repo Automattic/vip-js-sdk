f70840505f690b74393e60d8e046f744ba2e5a54
Nick Daugherty <ndaugherty987@gmail.com>
2015-04-30 14:31:17 -0600

    Add gulpfile for packaging up the source into distributable bundle

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require( 'gulp' );
+var browserify  = require( 'browserify' );
+var source      = require( 'vinyl-source-stream' );
+var uglify      = require( 'gulp-uglify' );
+var streamify   = require( 'gulp-streamify' );
+
+gulp.task( 'browserify', function() {
+    return browserify( './index.js' )
+        .bundle()
+        .pipe( source( 'vip-js-sdk.js' ) )
+        .pipe( streamify( uglify() ) )
+        .pipe( gulp.dest( './dist/' ) );
+});
