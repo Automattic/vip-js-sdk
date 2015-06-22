a1ba7b4aa86ba21e3cf452f99d836ef838759892
Nick Daugherty <ndaugherty987@gmail.com>
2015-06-22 10:33:59 -0600

    Fix how Request methods are called (pass them a 'this' arg)
    Needed so `this` is set to the right context when running

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ WPCOM_VIP.prototype.API_VERSION = '1';
 WPCOM_VIP.prototype.API_TIMEOUT = 10000;
 
 WPCOM_VIP.prototype.get = function() {
-    return this.req.get.apply( arguments );
+    return this.req.get.apply( this.req, arguments );
 };
 
 WPCOM_VIP.prototype.post = function() {
-    return this.req.post.apply( arguments );
+    return this.req.post.apply( this.req, arguments );
 };
 
 WPCOM_VIP.prototype.put = function() {
-    return this.req.get.apply( arguments );
+    return this.req.get.apply( this.req, arguments );
 };
 
 WPCOM_VIP.prototype.delete = function() {
-    return this.req.delete.apply( arguments );
+    return this.req.delete.apply( this.req, arguments );
 };
 
 module.exports = WPCOM_VIP;
