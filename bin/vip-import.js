c2eaf437e8f410db44c0d38069ddca6e7a16a502
Nick Daugherty <ndaugherty987@gmail.com>
2015-07-09 17:31:27 -0600

    Make sql option optional

--- bin/vip-import.js
+++ bin/vip-import.js
@@ -2,5 +2,5 @@ var program = require( 'commander' );
 
 program
     .version( '0.0.1' )
-    .option( '--sql <sql>', 'Path to sql file' )
+    .option( '--sql [sql]', 'Path to sql file' )
     .parse( process.argv );
