2e63b63ae460e4d8e9f3dbc267bd25b9290aaa33
Josh Betz <j@joshbetz.com>
2017-04-05 15:49:45 +0000

    Add sites.all() to list all sites for a given query

--- examples/list-sites/index.js
+++ examples/list-sites/index.js
@@ -1,7 +1,14 @@
 const vip = require( '../..' );
 const api = new vip();
 
+// Get all launched sites
+api.sites.all( { launched: true } )
+	.then( s => console.log( "Total launched sites:", s.length ) )
+	.catch( e => console.log( e ) );
+
+// Get first launched site
 api.sites
-	.query( { pagesize: 1 } )
+	.query( { pagesize: 1, launched: true } )
 	.then( body => console.log( body ) )
 	.catch( err => console.error( err ) );
+
